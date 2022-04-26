86b85d05a6967a14df456e4e152c31f8296a904d
Maxwell Snyder <55818423+maxsspot@users.noreply.github.com>
2022-04-26 11:57:24 -0400

    Update script.js

--- script.js
+++ script.js
@@ -148,12 +148,16 @@ const number = Math.floor(Math.random()*35) + 1;
                         var numer = 0;
     if (event.code === 'Enter' && numer == 0) {
         tryguess();
+                numer++;
     } else if (event.code === 'Enter' && numer == 1) {
         tryguess2();
+                numer++;
     } else if (event.code === 'Enter' && numer == 2) {
         tryguess3();
+                numer++;
     } else if (event.code === 'Enter' && numer == 3) {
         tryguess4();
+                numer++;
     } else if (event.code === 'Enter' && numer == 4) {
         tryguess5();
     } 
