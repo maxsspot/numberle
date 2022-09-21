de38e9a06d168dc253b62fbb1086504cbbd2a71a
Maxwell Snyder <55818423+maxsspot@users.noreply.github.com>
2022-09-21 10:12:08 -0400

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -2,7 +2,7 @@ var staticCacheName = "numerale1.1";
 
 self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open('your-cache-name').then(function(cache) {
+    caches.open('numerale1.1').then(function(cache) {
       return cache.addAll([
         '/',
       ])
