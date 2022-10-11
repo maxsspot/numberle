bfc7815767355f8aefb5f7b2f84495a14346aa08
Maxwell Snyder <55818423+maxsspot@users.noreply.github.com>
2022-10-11 10:01:45 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 {
-setTimeout(function(e){ if (location.hostName || document.domain) {e.preventDefault(); } }, 1);
+setTimeout(function(e){ if (location.hostname || document.domain) {e.preventDefault(); } }, 1);
       
             document.getElementById ("one").focus();
 const number = Math.floor(Math.random()*35) + 1;
