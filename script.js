15edd2e3021dec2846129d9eda5b1f6b71b3c918
Maxwell Snyder <55818423+maxsspot@users.noreply.github.com>
2022-12-16 12:35:53 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 ( () => {
             var x = 0;
             var winsye = localStorage.setItem ("winscount", x)
-            document.getElementById ("wins").value = winsye;
+            document.getElementById ("wins").innerHTML = winsye;
             document.getElementById ("one").focus();
 const number = Math.floor(Math.random()*35) + 1;
             function restart () {
