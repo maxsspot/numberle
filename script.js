1567b48fc859e7aacf4e0e6c672ed2eee4f061ef
Maxwell Snyder <55818423+maxsspot@users.noreply.github.com>
2023-02-17 18:10:15 -0500

    Update script.js

--- script.js
+++ script.js
@@ -210,7 +210,13 @@ for (var i = 0; i < items.length; i++) {
 } */
   
   document.querySelector("input").addEventListener("keydown", function(event) {
-  if (event.key === "e" || event.key === "-") {
+    var ye = document.getElementById("one").value;
+    var ye2 = document.getElementById("two").value;
+    var ye3 = document.getElementById("three").value;
+    var ye4 = document.getElementById("four").value;
+    var ye5 = document.getElementById("five").value;
+    
+  if (event.key === "e" || event.key === "-" || ye.value == 0 || ye.value > 35) {
     event.preventDefault();
   }
 });
