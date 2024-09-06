31041aa2b89dfd6da97e4ac8fcd99d3c8a64dc9b
nope <55818423+maxsspot@users.noreply.github.com>
2024-09-06 19:59:11 -0400

    Update script.js

--- script.js
+++ script.js
@@ -26,19 +26,19 @@ mode.addEventListener('change', function() {
 if (window.location.pathname == "/index" || window.location.pathname == "/index.html" || window.location.pathname == "/") {
       var number = Math.floor(Math.random()*35) + 1;
       modeCurrent = "normal";
-      normalMode.selected();
+      normalMode.selected = true;
 } else if (window.location.pathname == "/timed" || window.location.pathname == "/timed.html") {
       var number = Math.floor(Math.random()*35) + 1;
       modeCurrent = "normal";
-      timedNormalMode.selected();
+      timedNormalMode.selected = true;
 } else if (window.location.pathname == "/challenge" || window.location.pathname == "/challenge.html") {
       var number = Math.floor(Math.random()*50) + 1;
       modeCurrent = "challenge";
-      challengeMode.selected();
+      challengeMode.selected = true;
 } else if (window.location.pathname == "/timed-challenge" || window.location.pathname == "/timed-challenge.html") {
       var number = Math.floor(Math.random()*50) + 1;
       modeCurrent = "challenge";
-      timedChallengeMode.selected();
+      timedChallengeMode.selected = true;
 }
 
 ///////////////////////////////////////////////
