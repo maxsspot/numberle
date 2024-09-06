32aed9b2544e75b03ef17b89afdb64a2aaca33ce
nope <55818423+maxsspot@users.noreply.github.com>
2024-09-06 19:36:36 -0400

    Update timer.js

--- timer.js
+++ timer.js
@@ -1,3 +1,12 @@
+var normalMode = document.createElement ("option");normalMode.value = "Normal Mode";normalMode.text = "Normal Mode";mode.appendChild(normalMode);      
+var timedNormalMode = document.createElement ("option");timedNormalMode.value = "Timed Normal Mode";timedNormalMode.text = "Timed Normal Mode";mode.appendChild(timedNormalMode);
+var challengeMode = document.createElement ("option");challengeMode.value = "Challenge Mode";challengeMode.text = "Challenge Mode";mode.appendChild(challengeMode);
+var timedChallengeMode = document.createElement ("option");timedChallengeMode.value = "Timed Challenge Mode";timedChallengeMode.text = "Timed Challenge Mode";mode.appendChild(timedChallengeMode);
+
+////////////////////////////////
+////////////////////////////////
+////////////////////////////////
+
 var modeCurrent;
 
 var time = document.getElementById ("timer");
