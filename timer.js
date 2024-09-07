542e70c21b1d75a008d19c44e07fe076d8a811f1
nope <55818423+maxsspot@users.noreply.github.com>
2024-09-06 20:00:47 -0400

    Update timer.js

--- timer.js
+++ timer.js
@@ -1,3 +1,25 @@
+if (window.location.pathname == "/index" || window.location.pathname == "/index.html" || window.location.pathname == "/") {
+      var number = Math.floor(Math.random()*35) + 1;
+      modeCurrent = "normal";
+      normalMode.selected = true;
+} else if (window.location.pathname == "/timed" || window.location.pathname == "/timed.html") {
+      var number = Math.floor(Math.random()*35) + 1;
+      modeCurrent = "normal";
+      timedNormalMode.selected = true;
+} else if (window.location.pathname == "/challenge" || window.location.pathname == "/challenge.html") {
+      var number = Math.floor(Math.random()*50) + 1;
+      modeCurrent = "challenge";
+      challengeMode.selected = true;
+} else if (window.location.pathname == "/timed-challenge" || window.location.pathname == "/timed-challenge.html") {
+      var number = Math.floor(Math.random()*50) + 1;
+      modeCurrent = "challenge";
+      timedChallengeMode.selected = true;
+}
+
+////////////////////////////////
+////////////////////////////////
+////////////////////////////////
+
 var mode = document.getElementById ("modechooser");
 
 var normalMode = document.createElement ("option");normalMode.value = "Normal Mode";normalMode.text = "Normal Mode";mode.appendChild(normalMode);      
