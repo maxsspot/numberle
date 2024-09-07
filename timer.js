ad26e2938f773922bcd0923c28c6dbd7f84d600b
nope <55818423+maxsspot@users.noreply.github.com>
2024-09-06 20:02:04 -0400

    Update timer.js

--- timer.js
+++ timer.js
@@ -1,3 +1,10 @@
+var mode = document.getElementById ("modechooser");
+
+var normalMode = document.createElement ("option");normalMode.value = "Normal Mode";normalMode.text = "Normal Mode";mode.appendChild(normalMode);      
+var timedNormalMode = document.createElement ("option");timedNormalMode.value = "Timed Normal Mode";timedNormalMode.text = "Timed Normal Mode";mode.appendChild(timedNormalMode);
+var challengeMode = document.createElement ("option");challengeMode.value = "Challenge Mode";challengeMode.text = "Challenge Mode";mode.appendChild(challengeMode);
+var timedChallengeMode = document.createElement ("option");timedChallengeMode.value = "Timed Challenge Mode";timedChallengeMode.text = "Timed Challenge Mode";mode.appendChild(timedChallengeMode);
+
 if (window.location.pathname == "/index" || window.location.pathname == "/index.html" || window.location.pathname == "/") {
       var number = Math.floor(Math.random()*35) + 1;
       modeCurrent = "normal";
@@ -15,17 +22,6 @@ if (window.location.pathname == "/index" || window.location.pathname == "/index.
       modeCurrent = "challenge";
       timedChallengeMode.selected = true;
 }
-
-////////////////////////////////
-////////////////////////////////
-////////////////////////////////
-
-var mode = document.getElementById ("modechooser");
-
-var normalMode = document.createElement ("option");normalMode.value = "Normal Mode";normalMode.text = "Normal Mode";mode.appendChild(normalMode);      
-var timedNormalMode = document.createElement ("option");timedNormalMode.value = "Timed Normal Mode";timedNormalMode.text = "Timed Normal Mode";mode.appendChild(timedNormalMode);
-var challengeMode = document.createElement ("option");challengeMode.value = "Challenge Mode";challengeMode.text = "Challenge Mode";mode.appendChild(challengeMode);
-var timedChallengeMode = document.createElement ("option");timedChallengeMode.value = "Timed Challenge Mode";timedChallengeMode.text = "Timed Challenge Mode";mode.appendChild(timedChallengeMode);
 
 ////////////////////////////////
 ////////////////////////////////
