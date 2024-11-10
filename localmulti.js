f0b8366823fd10faf66a9d6f10f82ee5914119c8
nope <55818423+maxsspot@users.noreply.github.com>
2024-11-10 17:19:24 -0500

    Update localmulti.js

--- localmulti.js
+++ localmulti.js
@@ -4,6 +4,9 @@ var normalMode = document.createElement ("option");normalMode.value = "Normal Mo
 var timedNormalMode = document.createElement ("option");timedNormalMode.value = "Timed Normal Mode";timedNormalMode.text = "Timed Normal Mode";mode.appendChild(timedNormalMode);
 var challengeMode = document.createElement ("option");challengeMode.value = "Challenge Mode";challengeMode.text = "Challenge Mode";mode.appendChild(challengeMode);
 var timedChallengeMode = document.createElement ("option");timedChallengeMode.value = "Timed Challenge Mode";timedChallengeMode.text = "Timed Challenge Mode";mode.appendChild(timedChallengeMode);
+var localMultiplayer = document.createElement ("option");timedChallengeMode.value = "Local Multiplayer";timedChallengeMode.text = "Local Multiplayer";mode.appendChild(timedChallengeMode);
+
+localMultiplayer.selected = true;
 
 // Performs actions depending on the mode
 mode.addEventListener('change', function() {
