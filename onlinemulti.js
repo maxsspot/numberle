c3e30a35220bf569bc70f499ee4278c498f4a03c
nope <55818423+maxsspot@users.noreply.github.com>
2024-12-03 13:40:05 -0500

    Update onlinemulti.js

--- onlinemulti.js
+++ onlinemulti.js
@@ -25,77 +25,8 @@
 		<title>Numerale Local Multiplayer</title>
 	</head>
 	<body oncontextmenu = "return false;" id = "main" >
-	<!--<div id = "settingsMenu">
-		<br>
-		<h1>SETTINGS</h1>
-		<br><!-------------------->
-		<h2>Number of Players</h2>
-		<br>
-		<label for = "playerNumber">Players: </label>
-		<select id = "playerNumber" onchange = "updatePlayers()">
-			<option value = "2">2 Players</option>
-			<option value = "3">3 Players</option>
-			<option value = "4">4 Players</option>
-			<option value = "5">5 Players</option>
-		</select>
-		<br><br><!-------------------->
-		<h2>Player Names</h2>
-		<br>
-		<input id = "p1name" value = "Player 1" placeholder = "Player 1 name..." /><br id = "p1br">
-		<input id = "p2name" value = "Player 2" placeholder = "Player 2 name..." /><br id = "p2br">
-		<input id = "p3name" value = "Player 3" placeholder = "Player 3 name..." /><br id = "p3br">
-		<input id = "p4name" value = "Player 4" placeholder = "Player 4 name..." /><br id = "p4br">
-		<input id = "p5name" value = "Player 5" placeholder = "Player 5 name..." /><br id = "p5br">
-		<br><!-------------------->
-		<h2>Miscellaneous</h2>
-		<br>
-		<label>Maximum Number: </label>
-		<input id = "maxNumber" type = "number" value = "50" />
-		<!--<br><br>
-		<input id = "useIneq" type = "checkbox" />
-		<label for = "useIneq">Show the numbers range in an inequality</label>-->
-		<br><br><!-------------------->
-		<input id = "saveOptions" type = "checkbox" />
-		<label for = "saveOptions">Save and skip this in the future</label>
-		<br><br>
-		<span id = "apply" onclick = "applySettings()">START</span>
-	</div>-->
-		<nav>
-        <br>
-        <a id = "currentPlaying" style = "cursor:default;color:white;float:left;margin-left:15px;"><span id = "currentPlayer"></span>, go!</a>
-		<select style = "float:right;color:white;background-color:transparent;border:transparent;margin-right:15px;cursor:pointer;" id = "modechooser">
-			<!--<option style = "color:black;" value = "Normal Mode" selected>Normal Mode</option>
-			<option style = "color:black;" value = "Timed Normal Mode">Timed Normal Mode</option>
-			<option style = "color:black;" value = "Challenge Mode">Challenge Mode</option>
-			<option style = "color:black;" value = "Timed Challenge Mode">Timed Challenge Mode</option>-->
-		</select>
-			<br>
-        <br>
-        <hr>
-		</nav>
-		<main>
-        <br>
-        <h1>Numerale</h1>
-        <br>
-		<input type = "number" id = "multiBox" aria-label = "Enter your number guess." />
-        
-        <br>
-        <br>
-	<div id = "buttons">
-<!--<button id = "go" onclick = "makeAGuess()">Guess</button>-->
-		<span id = "inequality"><p><span id = "lowest">0</span> ≤ x ≤ <span id = "highest">100</span></p></span>
-		<span id = "words"><p>More than <span id = "lowestWords">0</span><br>Less than <span id = "highestWords">50</span></p></span>
-		</div>
-		<br>
-	<hr>
-		<br>
-		<button onclick = "resetSettings()" id = "resetSettings">Reset Settings</button>
-		</main>
-		<br>
-		<br>
-		<!--<sub><a href = "/keyboard-shortcuts">Keyboard Shortcuts</a>• <a href = "/availability">Ways to play</a></sub>-->
-		<br>
-<br>
-
+	<h1>Numerale</h1>
+	<button class = "onlineMenu">Join a room</button>
+	<button class = "onlineMenu">Create a room</button>
     </body>
 </html>
