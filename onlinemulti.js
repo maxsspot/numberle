9f4d8a9d57a3adc00e6808c76ba78f9295235845
nope <55818423+maxsspot@users.noreply.github.com>
2024-12-04 18:05:35 -0500

    Update onlinemulti.js

--- onlinemulti.js
+++ onlinemulti.js
@@ -1,11 +1,15 @@
 var menu = document.getElementById("mainMenu");
 var joinRoom = document.getElementById ("joinARoom");
 var createRoom = document.getElementById ("createARoom");
+var changeUsername = document.getElementById ("changeUsername");
+var username = document.getElementById ("username");
+var changeUserButton = document.getElementById ("confirmNameChange");
 
 function hideAll () {
   menu.style.display = "none";
   joinRoom.style.display = "none";
   createARoom.style.display = "none";
+  changeUsername.style.display = "none";
 }
 
 function returnHome () {
@@ -21,4 +25,21 @@ function openJoin () {
 function openCreate () {
   hideAll();
   createRoom.style.display = "block";
+}
+
+function openChange () {
+  hideAll();
+  changeUsername.style.display = "block";
+}
+
+function checkForDisallowed () {
+  if (username.value.include ("<") || username.value.includes (">")) {
+    username.style.borderBottom = "red solid 1px";
+    changeUserButton.style.color = "grey";
+    changeUserButton.disabled = true;
+  } else {
+    username.style.borderBottom = "white solid 1px";
+    changeUserButton.style.color = "white";
+    changeUserButton.disabled = false;
+  }
 }
