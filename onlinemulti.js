0872d9bc8fed8b38dcabb25b32da79133a2a6014
nope <55818423+maxsspot@users.noreply.github.com>
2024-12-06 14:36:49 -0500

    Update onlinemulti.js

--- onlinemulti.js
+++ onlinemulti.js
@@ -12,6 +12,7 @@ var roomCode;
 var roomCodeText;
 
 window.onload = function(){
+  console.log("SCRIPT LOADED!");
   menu = document.getElementById("mainMenu");
   joinRoom = document.getElementById ("joinARoom");
   createRoom = document.getElementById ("createARoom");
