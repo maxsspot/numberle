a5824eb6b9caa8afd4fdc37ba1d3f916eb77010c
nope <55818423+maxsspot@users.noreply.github.com>
2024-12-07 11:22:38 -0500

    Update onlinemulti.js

--- onlinemulti.js
+++ onlinemulti.js
@@ -20,7 +20,7 @@ document.getElementById("changeUsernameB").addEventListener("click", openChange)
 document.getElementById("confirmNameChange").addEventListener("click", confirmSettingsF);
 
 import { database } from "./firebaseConfig.js";
-import { getDatabase, ref, set, get, onValue, update} from "https://www.gstatic.com/firebasejs/11.0.2/firebase-database.js";
+import { firebase, getDatabase, ref, set, get, onValue, update } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-database.js";
 
 window.onload = function(){
   menu = document.getElementById("mainMenu");
