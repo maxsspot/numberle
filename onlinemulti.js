7236c1b56ea2fe04ffae862948cc10fee43ae911
nope <55818423+maxsspot@users.noreply.github.com>
2024-12-13 20:57:11 -0500

    Update onlinemulti.js

--- onlinemulti.js
+++ onlinemulti.js
@@ -412,7 +412,7 @@ function startGame() {
           chatStuff.style.display = "block";
         
           document.getElementById("gameStarts").style.display = "none";
-          document.getElementById("chatStuff").style.marginTop = "-3px";
+          document.getElementById("chatStuff").style.marginTop = "-5px";
       },500);
       
       inGame=true;
